6667fe9891350b00596a8a335a68261c1f5ab504
Evgeni Kolev <evgenysw@gmail.com>
2017-08-07 12:58:48 +0300

    Make compile errors fatal

--- test/make-compile.el
+++ test/make-compile.el
@@ -1,4 +1,7 @@
 (setq files '("evil-lion.el"))
 (setq byte-compile--use-old-handlers nil)
 (setq byte-compile-error-on-warn t)
-(mapc #'byte-compile-file files)
+(mapc (lambda (file)
+        (unless (byte-compile-file file)
+          (kill-emacs 1)))
+      files)
